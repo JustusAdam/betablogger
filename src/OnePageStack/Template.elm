eaa54e475f250f33c356270cf81708ef426043a3
Justus Adam <dev@justus.science>
2016-01-29 20:13:28 +0100

    Extra information access to AppInterface to Templates and access to url to component acquirers

--- src/OnePageStack/Template.elm
+++ src/OnePageStack/Template.elm
@@ -4,13 +4,14 @@ import Dict
 import Task exposing (Task)
 import Html exposing (Html)
 import OnePageStack.Types exposing (..)
+import Erl
 
 
 type alias Component = Maybe Html
-type alias AcquireComponent = Task String Component
+type alias AcquireComponent = Erl.Url -> Task String Component
 type alias Components = Dict.Dict String AcquireComponent
-type alias TemplateBuilder a = Components -> Html -> Task String a
-type alias Template = Html -> Task String Html
+type alias TemplateBuilder a = Components -> AppInterface -> Html -> Task String a
+type alias Template = AppInterface -> Html -> Task String Html
 
 
 withComponents : Components -> TemplateBuilder Html -> Template
@@ -18,27 +19,47 @@ withComponents c tb = tb c
 
 
 getComponent : String -> TemplateBuilder Component
-getComponent selector components html =
+getComponent selector components {currentUrl} _ =
   case Dict.get selector components of
-    Just acq -> acq
+    Just acq -> acq currentUrl
     Nothing -> Task.succeed Nothing
 
 
+getInterface : TemplateBuilder AppInterface
+getInterface _ i _ = Task.succeed i
+
+
+getUrl : TemplateBuilder Erl.Url
+getUrl = map .currentUrl getInterface
+
+
+getNavigator : TemplateBuilder Targeter
+getNavigator = map .navigator getInterface
+
+
+getCanvas : TemplateBuilder ContentHook
+getCanvas = map .canvas getInterface
+
+
+map : (a -> b) -> TemplateBuilder a -> TemplateBuilder b
+map f tb comp i h = Task.map f <| tb comp i h
+
+
 andThen : TemplateBuilder a -> (a -> TemplateBuilder b) -> TemplateBuilder b
-andThen ta f comp html = ta comp html `Task.andThen` \a -> f a comp html
+andThen ta f comp i html = ta comp i html `Task.andThen` \a -> f a comp i html
 
 
 render : (Html -> a) -> TemplateBuilder a
-render a _ h = Task.succeed (a h)
+render a _ _ h = Task.succeed (a h)
 
 
 return : a -> TemplateBuilder a
-return a _ _ = Task.succeed a
+return a _ _ _ = Task.succeed a
 
 
 acquired : Html -> AcquireComponent
-acquired = Task.mapError toString << Task.succeed << Just
+acquired html _ = Task.mapError toString <| Task.succeed <| Just html
 
 
 withTemplate : Template -> ProviderFunc -> ProviderFunc
-withTemplate t f i = f i `Task.andThen` t
+withTemplate t f i = f i `Task.andThen` t i
